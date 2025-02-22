a6b4610d9f1304ff77f01f14f228b6e9c9fd1f06
JakubTeichman <jteichman@student.agh.edu.pl>
2025-02-22 22:46:51 +0100

    update

---
+++ src/components/Heading/Heading.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+
+const ComponentName = () => {
+  const [state, setState] = useState(null);
+
+  return (
+    <>
+        <a href="">Home</a>
+        <a href="">About</a>
+        <a href="">Projects</a>
+        <a href="">Contact</a>
+    </>
+  );
+};
+
+export default ComponentName;
---
+++ src/components/Project/Project.jsx
@@ -0,0 +1,15 @@
+import React, {useState} from "react";
+
+const Project = () => {
+
+
+
+
+  return (
+    <>
+        <article>
+            <h2></h2>
+        </article>
+    </>
+  );
+}
