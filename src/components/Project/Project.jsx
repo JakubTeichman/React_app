bc1431610871c14d7ada0bc1d990b0991ab559bb
JakubTeichman <jteichman@student.agh.edu.pl>
2025-03-05 21:37:23 +0100

    update

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -16,17 +16,17 @@ function importAll(r) {
 const images = importAll(require.context('../../static', true, /\.(png|jpe?g|svg)$/));
 
 const Project1 = {
-  title: "Project 1",
-  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam.",
-  category: "category",
+  title: "Classification of heart disease",
+  description: "This project utilizes machine learning to classify heart diseases based on medical data. It analyzes various patient attributes, such as age, blood pressure, and cholesterol levels, to predict the risk of heart disease. The model is trained on real-world data and visualized to better understand the correlations between different variables.",
+  category: "Heart disease classification based on medical data",
   sourceCode: "https://github.com/JakubTeichman/Heart_disease_ML_model",
   images: images.project1
 }
 
 const Project2 = {
-  title: "Project 2",
-  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam.",
-  category: "category",
+  title: "Bullbozer price prediction",
+  description: "This project predicts the prices of used bulldozers using machine learning algorithms. It examines historical transactions and key equipment features, such as production year, operating hours, and model type, to estimate the market value of machines. The model helps businesses make informed buying and selling decisions.",
+  category: "Bullbozer price prediction using machine learning",
   sourceCode: "https://github.com/JakubTeichman/Bullbozer_price_prediction_ML_project",
   images: images.project2
 }
@@ -56,23 +56,25 @@ const Project = () => {
   };
 
   return (
-  
-    <div className="project" id="projects">
-      {projects.map((project, key) => (
-          <article>
-            <h1>{project.title}</h1>
-            <div>
-              <p>{project.category}</p>
-              {project.images && project.images.map((src, index) => (
-                <img key={index} src={src} alt={`Image ${index + 1}`} />
-              ))}
-            </div>
-            <p>{project.description}</p>
-            <button onClick={() => handleButtonClick(project.sourceCode)}>Source code</button>
-          </article>
-      ))}
-    </div>
+    <>
+       <h2 id="projects">MACHINE LEARNING PROJECTS</h2>
+      <div className="project">
+        {projects.map((project, key) => (
+            <article>
+              <h1>{project.title}</h1>
+              <div>
+                {project.images && project.images.map((src, index) => (
+                  <img key={index} src={src} alt={`Image ${index + 1}`} />
+                ))}
+              </div>
+              <p>{project.description}</p>
+              <button onClick={() => handleButtonClick(project.sourceCode)}>Source code</button>
+            </article>
+        ))}
+      </div>
     
+    </>
+   
   );
 }
 
--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -2,23 +2,29 @@ import { useState } from "react";
 import "./Slider.css";
 
 export default function Slider() {
-  const images = [
-    "https://github.com/JakubTeichman/PowerBI-raports/blob/main/Car%20accidents/Car%20accidents%20raport.jpg?raw=true",
-    "https://github.com/JakubTeichman/PowerBI-raports/blob/main/Car%20accidents/Car%20accidents%20raport.jpg"
-  ];
+  const images = {
+    "Car Accidents Report": "https://github.com/JakubTeichman/PowerBI-raports/blob/main/Car%20accidents/Car%20accidents%20raport.jpg?raw=true",
+    "Nuclear plants Report": "https://github.com/JakubTeichman/PowerBI-raports/blob/main/Nuclear%20plants/Nuclear_Plant_Raport_page-0001.jpg?raw=true"
+  };
 
+  const imageTitles = Object.keys(images);
   const [index, setIndex] = useState(0);
 
-  const nextSlide = () => setIndex((prev) => (prev + 1) % images.length);
-  const prevSlide = () => setIndex((prev) => (prev - 1 + images.length) % images.length);
+  const nextSlide = () => setIndex((prev) => (prev + 1) % imageTitles.length);
+  const prevSlide = () => setIndex((prev) => (prev - 1 + imageTitles.length) % imageTitles.length);
 
   return (
-    <div className="slider-container">
-      <div className="slider">
-        <img src={images[index]} alt="Slide" />
-        <button onClick={prevSlide} className="slider-btn prev-btn">←</button>
-        <button onClick={nextSlide} className="slider-btn next-btn">→</button>
-      </div>
-    </div>
+    <>
+        <h2 id="raports">POWER BI RAPORTS</h2>
+        <div className="slider-container">
+        <h2>{imageTitles[index]}</h2>
+        <div className="slider">
+            <img src={images[imageTitles[index]]} alt={imageTitles[index]} />
+            <button onClick={prevSlide} className="slider-btn prev-btn">←</button>
+            <button onClick={nextSlide} className="slider-btn next-btn">→</button>
+        </div>
+        </div>
+    </>
+    
   );
 }
